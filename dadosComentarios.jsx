7a19247cd101812a41603a2682471e3b396458fc
leandrocastro713 <109395934+leandrocastro713@users.noreply.github.com>
2023-01-18 21:45:55 -0300

    Initial commit

---
+++ dadosComentarios.jsx
@@ -0,0 +1,29 @@
+const dadosComentarios = [
+  {
+    id_c: 1,
+    titulo: "primeiytro",
+    subtitulo: "primecasiro comentario"
+  },
+  {
+    id_c: 2,
+    titulo: "segundo",
+    subtitulo: "seg comentario"
+  },
+  {
+    id_c: 3,
+    titulo: "terceiro",
+    subtitulo: "terce comentario"
+  },
+  {
+    id_c: 4,
+    titulo: "QUARTO",
+    subtitulo: "qua comentario"
+  },
+  {
+    id_c: 5,
+    titulo: "QUINTO",
+    subtitulo: "qui comentario"
+  }
+];
+
+export default dadosComentarios;
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Botao from "./components/Botao/Botao";
+import Comentario from "./components/Comentario/Comentario";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <Botao />
+      <Comentario />
+    </>
+  );
+}
